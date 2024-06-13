11eb7e12933673e0d1e724d862401893a4fe961b
yebyyy <yexiaomeng2004@gmail.com>
2024-06-13 13:10:45 -0500

    frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div>
+        <h1 className="bg-blue-500 text-whit">hello world</h1>
+      </div>
+  );
+}
+
+export default App;
